adfad1e327d585d2d401b339c727720fedf8f3da
unladen <unladen@dfm.github.io>
2021-05-17 02:05:45 +0000

    deploy d1610fa73166c223b0f9fbdd0bf4765710c19bda

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1rc2.dev3+ge02b743',
+    VERSION: '0.0.1rc2.dev4+gd1610fa',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
